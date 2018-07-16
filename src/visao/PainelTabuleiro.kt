8da4fad02b116badd361a69bd3716491ae42bc7a
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-07-16 20:54:49 -0300

    Implementa parte visual do jogo

---
+++ src/visao/PainelTabuleiro.kt
@@ -0,0 +1,17 @@
+package visao
+
+import modelo.Tabuleiro
+import java.awt.GridLayout
+import javax.swing.JPanel
+
+class PainelTabuleiro(tabuleiro: Tabuleiro) : JPanel() {
+
+    init {
+        layout = GridLayout(tabuleiro.qtdeLinhas, tabuleiro.qtdeColunas)
+        tabuleiro.forEachCampo { campo ->
+            val botao = BotaoCampo(campo)
+            add(botao)
+        }
+    }
+
+}
